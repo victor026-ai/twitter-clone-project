dc5f67d9732741da9e7baca0f0a992e8dafd6d42
vicci <victorgeronimo515@gmail.com>
2025-02-26 18:34:13 +0800

    3 - notifications fetching and deletion added

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 import { IoSettingsOutline } from "react-icons/io5";
@@ -6,31 +9,44 @@ import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	const queryClient = useQueryClient();
+	const { data: notifications, isLoading } = useQuery({
+		queryKey: ["notifications"],
+		queryFn: async() => {
+			try {
+				const res = await fetch("/api/notifications");
+				const data = await res.json();
+				if(!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		}
+	});
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+	const { mutate: deleteNotifications } = useMutation({
+		mutationFn: async() => {
+			try {
+				const res = await fetch("/api/notifications", {
+					method: "DELETE"
+				})
+				const data = await res.json();
+				if(!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}	
+		},
+		onSuccess: () => {
+			toast.success("Notifications deleted successfully");
+			queryClient.invalidateQueries({ queryKey: ["notifications"] });
+		},
+		onError: () => {
+			toast.error(error.message);
+		}
+	})
 
 	return (
 		<>
